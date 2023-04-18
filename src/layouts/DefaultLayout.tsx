d31b32b3e0db7aba131273ce2bc4860d83879b93
mh-developer <mh7860@student.uni-lj.si>
2023-04-18 09:27:26 +0200

    chore: apply auth on whole default layout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar/Sidebar';
 import { Varela_Round } from '@next/font/google';
+import { useRequiredAuth } from '@/context/RequiredAuth';
 
 const varelaRound = Varela_Round({
   subsets: ['latin'],
@@ -9,6 +10,12 @@ const varelaRound = Varela_Round({
 });
 
 export default function DefaultLayout({ children }: any) {
+  const auth = useRequiredAuth();
+
+  if (!!!auth.user) {
+    return <></>;
+  }
+
   return (
     <div className={`${varelaRound.className} h-screen bg-gray-50 dark:bg-gray-900`}>
       <Sidebar />
