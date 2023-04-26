21a06a9323b8cc1562891151b09ce3991a10e97d
mh-developer <mh7860@student.uni-lj.si>
2023-04-26 12:12:50 +0200

    feat: add support for map component

---
+++ src/components/Maps/Map.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+interface MapboxMapProps {
+  initialOptions?: Omit<mapboxgl.MapboxOptions, 'container'>;
+  onCreated?(map: mapboxgl.Map): void;
+  onLoaded?(map: mapboxgl.Map): void;
+  onRemoved?(): void;
+}
+
+function MapboxMap({ initialOptions = {}, onCreated, onLoaded, onRemoved }: MapboxMapProps) {
+  const [map, setMap] = useState<mapboxgl.Map>();
+
+  const mapNode = useRef(null);
+
+  useEffect(() => {
+    const node = mapNode.current;
+
+    if (typeof window === 'undefined' || node === null) return;
+
+    const mapboxMap = new mapboxgl.Map({
+      container: node,
+      accessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [15.646, 46.554],
+      zoom: 10,
+      ...initialOptions,
+    });
+
+    setMap(mapboxMap);
+    if (onCreated) onCreated(mapboxMap);
+
+    if (onLoaded) mapboxMap.once('load', () => onLoaded(mapboxMap));
+
+    return () => {
+      mapboxMap.remove();
+      setMap(undefined);
+      if (onRemoved) onRemoved();
+    };
+  }, []);
+
+  return <div ref={mapNode} style={{ width: '100%', height: '100%' }} />;
+}
+
+export default MapboxMap;
