2bd40c8f0b35763b38da23abac19f0978a795f6e
mh-developer <mh7860@student.uni-lj.si>
2023-04-28 00:19:06 +0200

    chore: update onboarding text

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -111,7 +111,7 @@ export default function Calibration() {
     <Auth>
       <ToastContainer />
       <div className="w-full max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow dark:bg-gray-800">
-        <h2 className="text-2xl font-bold text-gray-900 dark:text-white">Onboarding</h2>
+        <h2 className="text-2xl font-bold text-gray-900 dark:text-white">Dobrodošli! Še zadnji korak...</h2>
         <div className="text-sm font-medium text-gray-500 dark:text-gray-400">
           Prosimo, da naslednje podatke izpolnite z največjo možnjo natančnostjo. <br />
           To nam bo pomagalo, da vam zagotovimo najboljšo izkušnjo.
