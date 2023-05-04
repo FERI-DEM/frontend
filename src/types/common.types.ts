b0e6447d017a34d63f62f1d8a674d4e7e33315f7
mh-developer <mh7860@student.uni-lj.si>
2023-05-04 11:08:43 +0200

    feat: prepare date period selector ranges

--- src/types/common.types.ts
+++ src/types/common.types.ts
@@ -1,40 +1,54 @@
+import moment from 'moment';
+
 export interface ApiError {
-  error: string;
-  method: string;
-  path: string;
-  statusCode: number;
-  timestamp: string;
+    error: string;
+    method: string;
+    path: string;
+    statusCode: number;
+    timestamp: string;
 }
 
 export const dateRangeOptions = [
-  {
-    label: 'Danes',
-    callback() {
-      return { from: new Date(), to: new Date() };
+    {
+        label: 'Danes',
+        callback() {
+            return { from: moment().startOf('day').toDate(), to: moment().endOf('day').toDate() };
+        },
     },
-  },
-  {
-    label: 'Jutri',
-    callback() {
-      return { from: new Date(), to: new Date() };
+    {
+        label: 'Jutri',
+        callback() {
+            return {
+                from: moment().add(1, 'day').startOf('day').toDate(),
+                to: moment().add(1, 'day').endOf('day').toDate(),
+            };
+        },
     },
-  },
-  {
-    label: 'Včeraj',
-    callback() {
-      return { from: new Date(), to: new Date() };
+    {
+        label: 'Včeraj',
+        callback() {
+            return {
+                from: moment().add(-1, 'day').startOf('day').toDate(),
+                to: moment().add(-1, 'day').endOf('day').toDate(),
+            };
+        },
     },
-  },
-  {
-    label: 'Zadnjih 7 dni',
-    callback() {
-      return { from: new Date(), to: new Date() };
+    {
+        label: 'Zadnjih 7 dni',
+        callback() {
+            return {
+                from: moment().add(-5, 'day').startOf('day').toDate(),
+                to: moment().add(5, 'day').endOf('day').toDate(),
+            };
+        },
     },
-  },
-  {
-    label: 'Trenutni mesec',
-    callback() {
-      return { from: new Date(), to: new Date() };
+    {
+        label: 'Trenutni mesec',
+        callback() {
+            return {
+                from: moment().startOf('month').toDate(),
+                to: moment().endOf('month').endOf('day').toDate(),
+            };
+        },
     },
-  },
 ];
