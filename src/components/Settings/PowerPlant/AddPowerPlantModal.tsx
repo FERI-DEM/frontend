492df3176172ccac705dcf030159da7fafb50b98
mh-developer <mh7860@student.uni-lj.si>
2023-05-10 00:20:55 +0200

    fix: fix saving edit modal of power plants

--- src/components/Settings/PowerPlant/AddPowerPlantModal.tsx
+++ src/components/Settings/PowerPlant/AddPowerPlantModal.tsx
@@ -99,19 +99,7 @@ const AddPowerPlantModal = ({ closeModal, updatePowerPlants }: AddPowerPlantModa
                             className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
                             onClick={closeModal}
                         >
-                            <svg
-                                aria-hidden="true"
-                                className="w-5 h-5"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                    clipRule="evenodd"
-                                ></path>
-                            </svg>
+                            <span className="material-symbols-rounded">close</span>
                             <span className="sr-only">Close modal</span>
                         </button>
                     </div>
@@ -138,22 +126,6 @@ const AddPowerPlantModal = ({ closeModal, updatePowerPlants }: AddPowerPlantModa
                                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                     Lokacija
                                 </label>
-                                <input
-                                    type="hidden"
-                                    {...register('location.longitude', {
-                                        required: 'Lokacija elektrarne je obvezno polje',
-                                    })}
-                                    value={+lng}
-                                    required
-                                />
-                                <input
-                                    type="hidden"
-                                    {...register('location.latitude', {
-                                        required: 'Lokacija elektrarne je obvezno polje',
-                                    })}
-                                    value={+lat}
-                                    required
-                                />
                                 <div className="h-72 w-full">
                                     <MapboxMap
                                         initialOptions={{ center: [+lng, +lat], zoom: +zoom }}
