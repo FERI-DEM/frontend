22664be73dc1bcc0ee08f27c9052bbcb1fe391a0
mh-developer <mh7860@student.uni-lj.si>
2023-05-16 21:32:10 +0200

    chore: translate login & register page

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -31,7 +31,7 @@ export default function Login() {
     return (
         <Auth>
             <div className="w-full max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow dark:bg-gray-800">
-                <h2 className="text-2xl font-bold text-gray-900 dark:text-white">Sign in to platform</h2>
+                <h2 className="text-2xl font-bold text-gray-900 dark:text-white">Vpišite se v platformo</h2>
                 <FormProvider {...methods}>
                     <form className="mt-8 space-y-6" action="" onSubmit={handleSubmit(onSubmit)}>
                         <div>
@@ -39,60 +39,60 @@ export default function Login() {
                                 htmlFor="email"
                                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                             >
-                                Your email
+                                Elektronski naslov
                             </label>
                             <input
                                 type="email"
-                                {...register('email', { required: 'Email is required' })}
+                                {...register('email', { required: 'Elektronski naslov je obvezen' })}
                                 name="email"
                                 id="email"
                                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                placeholder="name@mail.com"
+                                placeholder="ime.priimek@mail.com"
                                 required
                             />
-                            {errors.email && <p className="text-red-400">{errors.email.message}</p>}
+                            {errors.email && <p className="mt-2 text-red-400">{errors.email.message}</p>}
                         </div>
                         <div>
                             <label
                                 htmlFor="password"
                                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                             >
-                                Your password
+                                Geslo
                             </label>
                             <input
                                 type="password"
-                                {...register('password', { required: 'Password is required' })}
+                                {...register('password', { required: 'Geslo je obvezno' })}
                                 name="password"
                                 id="password"
                                 placeholder="••••••••"
                                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                 required
                             />
-                            {errors.password && <p className="text-red-400">{errors.password.message}</p>}
+                            {errors.password && <p className="mt-2 text-red-400">{errors.password.message}</p>}
                         </div>
                         <div className="flex items-start">
                             <button
                                 type="submit"
                                 className="w-full px-5 py-3 text-base font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                             >
-                                Login to your account
+                                Prijavi se v svoj račun
                             </button>
                             <a
                                 href="#"
                                 className="ml-auto text-sm text-primary-700 hover:underline dark:text-primary-500"
                             >
-                                Lost Password?
+                                Pozabljeno geslo?
                             </a>
                         </div>
                         <div className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                            Not registered?{' '}
+                            Še nimate računa?{' '}
                             <Link
                                 href={{
                                     pathname: '/auth/register',
                                 }}
                                 className="text-primary-700 hover:underline dark:text-primary-500"
                             >
-                                Create account
+                                Ustvari račun
                             </Link>
                         </div>
                     </form>
@@ -138,7 +138,7 @@ export default function Login() {
                                 />
                             </g>
                         </svg>
-                        Sign in with Google
+                        Prijavi se z Google računom
                     </button>
                     <button
                         type="button"
@@ -151,7 +151,7 @@ export default function Login() {
                             <path fill="#05a6f0" d="M1 12h10v10H1z" />
                             <path fill="#ffba08" d="M12 12h10v10H12z" />
                         </svg>
-                        Sign in with Microsoft
+                        Prijavi se z Microsoft računom
                     </button>
                 </div>
             </div>
