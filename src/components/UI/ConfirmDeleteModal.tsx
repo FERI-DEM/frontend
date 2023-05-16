0bc1f2a6d049f0e55ea639d5d63a8cd9f2a2d558
andrej-koman <andrej.koman123@gmail.com>
2023-05-16 17:17:19 +0200

    fix: confirm centered

--- src/components/UI/ConfirmDeleteModal.tsx
+++ src/components/UI/ConfirmDeleteModal.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 const ConfirmDeleteModal = ({closeModal, deleteItem}: Props) => {
     return (
-        <div id="popup-modal" className="fixed top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div className="relative w-full max-w-md max-h-full">
+        <div id="popup-modal" className="fixed bg-opacity-40 bg-black top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem) h-full">
+            <div className="relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-md max-h-full">
                 <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
                     <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal">
                         <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
