5f47d84985f101d4d1ab59229f7a30fa49cb5a60
mh-developer <mh7860@student.uni-lj.si>
2023-05-16 20:17:55 +0200

    chore: reformat files

--- src/hooks/useAuthRequired.tsx
+++ src/hooks/useAuthRequired.tsx
@@ -3,13 +3,13 @@ import { useRouter } from 'next/router';
 import { useAuth } from '../context/AuthContext';
 
 export const useAuthRequired = () => {
-  const auth = useAuth();
-  const router = useRouter();
-  useEffect(() => {
-    if (!auth.loading && (auth.user === false || auth.user === null)) {
-      router.push('/auth/login');
-    }
-  }, []);
+    const auth = useAuth();
+    const router = useRouter();
+    useEffect(() => {
+        if (!auth.loading && (auth.user === false || auth.user === null)) {
+            router.push('/auth/login');
+        }
+    }, []);
 
-  return auth;
+    return auth;
 };
