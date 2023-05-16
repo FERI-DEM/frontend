06109aa47ee60f340e355fe96e6610e0373f0b0c
mh-developer <mh7860@student.uni-lj.si>
2023-05-16 09:53:56 +0200

    feat: support new date types & improve loading of chart data

--- src/components/Charts/ChartLine.tsx
+++ src/components/Charts/ChartLine.tsx
@@ -1,205 +1,250 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import dynamic from 'next/dynamic';
-import { ApexOptions } from 'apexcharts';
 
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
 export default function ChartLine({
-  dataset,
-  displayRange,
+    dataset,
+    displayRange,
 }: {
-  dataset: ApexAxisChartSeries | ApexNonAxisChartSeries | undefined;
-  displayRange?: { min?: number; max?: number };
+    dataset: ApexAxisChartSeries | undefined;
+    displayRange?: { min?: number; max?: number };
 }) {
-  let mainChartColors: any = {};
+    let mainChartColors: any = {};
 
-  if (document.documentElement.classList.contains('dark')) {
-    mainChartColors = {
-      borderColor: '#374151',
-      labelColor: '#9CA3AF',
-      opacityFrom: 0,
-      opacityTo: 0.15,
-    };
-  } else {
-    mainChartColors = {
-      borderColor: '#F3F4F6',
-      labelColor: '#6B7280',
-      opacityFrom: 0.45,
-      opacityTo: 0,
-    };
-  }
+    if (document.documentElement.classList.contains('dark')) {
+        mainChartColors = {
+            borderColor: '#374151',
+            labelColor: '#9CA3AF',
+            opacityFrom: 0,
+            opacityTo: 0.15,
+        };
+    } else {
+        mainChartColors = {
+            borderColor: '#F3F4F6',
+            labelColor: '#6B7280',
+            opacityFrom: 0.45,
+            opacityTo: 0,
+        };
+    }
 
-  const defaultOptions = () => {
-    return {
-      chart: {
-        defaultLocale: 'sl',
-        locales: [
-          {
-            name: 'sl',
-            options: {
-              months: [
-                ...Array.from(Array(12).keys()).map((month) => {
-                  let date = new Date();
-                  date.setDate(1);
-                  date.setMonth(month);
-                  return date.toLocaleString('sl-SI', { month: 'long' });
-                }),
-              ],
-              shortMonths: [
-                ...Array.from(Array(12).keys()).map((month) => {
-                  let date = new Date();
-                  date.setDate(1);
-                  date.setMonth(month);
-                  return date.toLocaleString('sl-SI', { month: 'short' });
-                }),
-              ],
-              days: [
-                ...Array.from(Array(7).keys()).map((month) => {
-                  let date = new Date();
-                  date.setDate(1);
-                  date.setMonth(month);
-                  return date.toLocaleString('sl-SI', { weekday: 'long' });
-                }),
-              ],
-              shortDays: [
-                ...Array.from(Array(7).keys()).map((month) => {
-                  let date = new Date();
-                  date.setDate(1);
-                  date.setMonth(month);
-                  return date.toLocaleString('sl-SI', { weekday: 'short' });
-                }),
-              ],
-              toolbar: {
-                download: 'Prenesi SVG',
-                selection: 'Izbor',
-                selectionZoom: 'Povečava izbora',
-                zoomIn: 'Približaj',
-                zoomOut: 'Oddalji',
-                pan: 'Premikanje',
-                reset: 'Ponastavi povečavo',
-              },
-            },
-          },
-        ],
-        height: 420,
-        type: 'area' as 'area',
-        fontFamily: 'Inter, sans-serif',
-        foreColor: mainChartColors.labelColor,
-        animations: {
-          enabled: false,
-        },
-        toolbar: {
-          show: true,
-        },
-      },
-      fill: {
-        type: 'gradient' as 'gradient',
-        gradient: {
-          opacityFrom: mainChartColors.opacityFrom,
-          opacityTo: mainChartColors.opacityTo,
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      tooltip: {
-        shared: true,
-        style: {
-          fontSize: '14px',
-          fontFamily: 'Inter, sans-serif',
-        },
-      },
-      grid: {
-        show: true,
-        borderColor: mainChartColors.borderColor,
-        strokeDashArray: 1,
-        padding: {
-          left: 35,
-          bottom: 15,
-        },
-      },
-      markers: {
-        size: 0,
-        strokeColors: '#ffffff',
-        hover: {
-          size: 5,
-          sizeOffset: 3,
-        },
-      },
-      xaxis: {
-        type: 'datetime' as 'datetime',
-        min: displayRange?.min ?? undefined,
-        max: displayRange?.max ?? undefined,
-        labels: {
-          style: {
-            colors: [mainChartColors.labelColor],
-            fontSize: '14px',
-            fontWeight: 500,
-          },
-        },
-        axisBorder: {
-          color: mainChartColors.borderColor,
-        },
-        axisTicks: {
-          color: mainChartColors.borderColor,
-        },
-        crosshairs: {
-          show: true,
-          position: 'back' as 'back',
-          stroke: {
-            color: mainChartColors.borderColor,
-            width: 1,
-            dashArray: 10,
-          },
-        },
-      },
-      yaxis: {
-        labels: {
-          style: {
-            colors: [mainChartColors.labelColor],
-            fontSize: '14px',
-            fontWeight: 500,
-          },
-          formatter: function (value: any) {
-            return value != null ? `${+value.toFixed(2)} kW` : '';
-          },
-        },
-      },
-      legend: {
-        fontSize: '14px',
-        fontWeight: 500,
-        fontFamily: 'Inter, sans-serif',
-        labels: {
-          colors: [mainChartColors.labelColor],
-        },
-        itemMargin: {
-          horizontal: 10,
-        },
-      },
-      responsive: [
-        {
-          breakpoint: 1024,
-          options: {
-            xaxis: {
-              labels: {
-                show: false,
-              },
-            },
-          },
-        },
-      ],
-    };
-  };
+    return (
+        <>
+            <Chart
+                options={{
+                    chart: {
+                        defaultLocale: 'sl',
+                        locales: [
+                            {
+                                name: 'sl',
+                                options: {
+                                    months: [
+                                        ...Array.from(Array(12).keys()).map((month) => {
+                                            let date = new Date();
+                                            date.setDate(1);
+                                            date.setMonth(month);
+                                            return date.toLocaleString('sl-SI', { month: 'long' });
+                                        }),
+                                    ],
+                                    shortMonths: [
+                                        ...Array.from(Array(12).keys()).map((month) => {
+                                            let date = new Date();
+                                            date.setDate(1);
+                                            date.setMonth(month);
+                                            return date.toLocaleString('sl-SI', { month: 'short' });
+                                        }),
+                                    ],
+                                    days: [
+                                        ...Array.from(Array(7).keys()).map((month) => {
+                                            let date = new Date();
+                                            date.setDate(1);
+                                            date.setMonth(month);
+                                            return date.toLocaleString('sl-SI', { weekday: 'long' });
+                                        }),
+                                    ],
+                                    shortDays: [
+                                        ...Array.from(Array(7).keys()).map((month) => {
+                                            let date = new Date();
+                                            date.setDate(1);
+                                            date.setMonth(month);
+                                            return date.toLocaleString('sl-SI', { weekday: 'short' });
+                                        }),
+                                    ],
+                                    toolbar: {
+                                        download: 'Prenesi SVG',
+                                        exportToCSV: 'Prenesi CSV',
+                                        exportToPNG: 'Prenesi PNG',
+                                        exportToSVG: 'Prenesi SVG',
+                                        selection: 'Izbor',
+                                        selectionZoom: 'Povečava izbora',
+                                        zoomIn: 'Približaj',
+                                        zoomOut: 'Oddalji',
+                                        pan: 'Premikanje',
+                                        reset: 'Ponastavi povečavo',
+                                    },
+                                },
+                            },
+                        ],
+                        height: 420,
+                        type: 'area' as 'area',
+                        fontFamily: 'Inter, sans-serif',
+                        foreColor: mainChartColors.labelColor,
+                        animations: {
+                            enabled: true,
+                        },
+                        toolbar: {
+                            show: true,
+                        },
 
-  const [options, setOptions] = useState<ApexOptions>(defaultOptions);
+                        events: {
+                            beforeZoom: (chartContext: any, { xaxis }: any) => {
+                                let dateFrom: Date = null as any;
+                                let dateTo: Date = null as any;
+                                dataset?.forEach((x) => {
+                                    if (x.data && x.data.length > 0) {
+                                        const dataFrom: any = x.data[0];
+                                        dateFrom =
+                                            dateFrom && new Date(dataFrom.x).getTime() - dateFrom.getTime() > 0
+                                                ? new Date(dataFrom.x)
+                                                : !dateFrom
+                                                ? new Date(dataFrom.x)
+                                                : dateFrom;
 
-  useEffect(() => {
-    setOptions(defaultOptions);
-  }, [displayRange]);
+                                        const dataTo: any = x.data[x.data.length - 1];
+                                        dateTo =
+                                            dateTo && new Date(dataTo.x).getTime() - dateTo.getTime() < 0
+                                                ? new Date(dataTo.x)
+                                                : !dateTo
+                                                ? new Date(dataTo.x)
+                                                : dateTo;
+                                    }
+                                });
 
-  return (
-    <>
-      <Chart options={options} series={dataset} type="area" height={420} width={'100%'} />
-    </>
-  );
+                                if (dateFrom && dateTo) {
+                                    let maindifference = new Date(dateTo).valueOf() - new Date(dateFrom).valueOf();
+                                    let zoomdifference = xaxis.max - xaxis.min;
+
+                                    if (zoomdifference > maindifference) {
+                                        return {
+                                            xaxis: {
+                                                min: dateFrom,
+                                                max: dateTo,
+                                            },
+                                        };
+                                    } else {
+                                        return {
+                                            xaxis: {
+                                                min: xaxis.min,
+                                                max: xaxis.max,
+                                            },
+                                        };
+                                    }
+                                }
+                            },
+                        },
+                    },
+                    fill: {
+                        type: 'gradient' as 'gradient',
+                        gradient: {
+                            opacityFrom: mainChartColors.opacityFrom,
+                            opacityTo: mainChartColors.opacityTo,
+                        },
+                    },
+                    dataLabels: {
+                        enabled: false,
+                    },
+                    tooltip: {
+                        shared: true,
+                        style: {
+                            fontSize: '14px',
+                            fontFamily: 'Inter, sans-serif',
+                        },
+                    },
+                    grid: {
+                        show: true,
+                        borderColor: mainChartColors.borderColor,
+                        strokeDashArray: 1,
+                        padding: {
+                            left: 35,
+                            bottom: 15,
+                        },
+                    },
+                    markers: {
+                        size: 0,
+                        strokeColors: '#ffffff',
+                        hover: {
+                            size: 5,
+                            sizeOffset: 3,
+                        },
+                    },
+                    xaxis: {
+                        type: 'datetime' as 'datetime',
+                        min: displayRange?.min ?? undefined,
+                        max: displayRange?.max ?? undefined,
+                        labels: {
+                            style: {
+                                colors: [mainChartColors.labelColor],
+                                fontSize: '14px',
+                                fontWeight: 500,
+                            },
+                        },
+                        axisBorder: {
+                            color: mainChartColors.borderColor,
+                        },
+                        axisTicks: {
+                            color: mainChartColors.borderColor,
+                        },
+                        crosshairs: {
+                            show: true,
+                            position: 'back' as 'back',
+                            stroke: {
+                                color: mainChartColors.borderColor,
+                                width: 1,
+                                dashArray: 10,
+                            },
+                        },
+                    },
+                    yaxis: {
+                        labels: {
+                            style: {
+                                colors: [mainChartColors.labelColor],
+                                fontSize: '14px',
+                                fontWeight: 500,
+                            },
+                            formatter: function (value: any) {
+                                return value != null ? `${+value.toFixed(2)} kW` : '';
+                            },
+                        },
+                    },
+                    legend: {
+                        fontSize: '14px',
+                        fontWeight: 500,
+                        fontFamily: 'Inter, sans-serif',
+                        labels: {
+                            colors: [mainChartColors.labelColor],
+                        },
+                        itemMargin: {
+                            horizontal: 10,
+                        },
+                    },
+                    responsive: [
+                        {
+                            breakpoint: 1024,
+                            options: {
+                                xaxis: {
+                                    labels: {
+                                        show: false,
+                                    },
+                                },
+                            },
+                        },
+                    ],
+                }}
+                series={dataset}
+                type="area"
+                height={420}
+                width={'100%'}
+            />
+        </>
+    );
 }
--- src/types/common.types.ts
+++ src/types/common.types.ts
@@ -8,47 +8,135 @@ export interface ApiError {
     timestamp: string;
 }
 
-export const dateRangeOptions = [
-    {
-        label: 'Danes',
-        callback() {
-            return { from: moment().startOf('day').toDate(), to: moment().endOf('day').toDate() };
+export enum DateType {
+    Default = 0,
+    Today = 1,
+    Yesterday = 2,
+    Tomorrow = 3,
+    CurrentWeek = 4,
+    LastWeek = 5,
+    NextWeek = 6,
+    CurrentMonth = 7,
+    LastMonth = 8,
+    CurrentYear = 9,
+    LastYear = 10,
+}
+
+export interface DateRangeOption {
+    label: string;
+    type: DateType;
+    callback: () => void;
+}
+
+export const dateRangeOptions = (filter?: number[]): DateRangeOption[] => {
+    const options = [
+        {
+            label: 'Privzeto obdobje',
+            type: DateType.Default,
+            callback() {
+                return {
+                    from: moment().add(-5, 'day').startOf('day').toDate(),
+                    to: moment().add(7, 'day').endOf('day').toDate(),
+                };
+            },
         },
-    },
-    {
-        label: 'Jutri',
-        callback() {
-            return {
-                from: moment().add(1, 'day').startOf('day').toDate(),
-                to: moment().add(1, 'day').endOf('day').toDate(),
-            };
+        {
+            label: 'Danes',
+            type: DateType.Today,
+            callback() {
+                return { from: moment().startOf('day').toDate(), to: moment().endOf('day').toDate() };
+            },
         },
-    },
-    {
-        label: 'Včeraj',
-        callback() {
-            return {
-                from: moment().add(-1, 'day').startOf('day').toDate(),
-                to: moment().add(-1, 'day').endOf('day').toDate(),
-            };
+        {
+            label: 'Jutri',
+            type: DateType.Tomorrow,
+            callback() {
+                return {
+                    from: moment().add(1, 'day').startOf('day').toDate(),
+                    to: moment().add(1, 'day').endOf('day').toDate(),
+                };
+            },
         },
-    },
-    {
-        label: 'Zadnjih 7 dni',
-        callback() {
-            return {
-                from: moment().add(-5, 'day').startOf('day').toDate(),
-                to: moment().add(5, 'day').endOf('day').toDate(),
-            };
+        {
+            label: 'Včeraj',
+            type: DateType.Yesterday,
+            callback() {
+                return {
+                    from: moment().add(-1, 'day').startOf('day').toDate(),
+                    to: moment().add(-1, 'day').endOf('day').toDate(),
+                };
+            },
         },
-    },
-    {
-        label: 'Trenutni mesec',
-        callback() {
-            return {
-                from: moment().startOf('month').toDate(),
-                to: moment().endOf('month').endOf('day').toDate(),
-            };
+        {
+            label: 'Trenutni teden',
+            type: DateType.CurrentWeek,
+            callback() {
+                return {
+                    from: moment().startOf('week').toDate(),
+                    to: moment().endOf('week').toDate(),
+                };
+            },
         },
-    },
-];
+        {
+            label: 'Naslednji teden',
+            type: DateType.NextWeek,
+            callback() {
+                return {
+                    from: moment().add(1, 'week').startOf('week').toDate(),
+                    to: moment().add(1, 'week').endOf('week').toDate(),
+                };
+            },
+        },
+        {
+            label: 'Prejšnji teden',
+            type: DateType.LastWeek,
+            callback() {
+                return {
+                    from: moment().add(-1, 'week').startOf('week').toDate(),
+                    to: moment().add(-1, 'week').endOf('week').toDate(),
+                };
+            },
+        },
+        {
+            label: 'Trenutni mesec',
+            type: DateType.CurrentMonth,
+            callback() {
+                return {
+                    from: moment().startOf('month').toDate(),
+                    to: moment().endOf('month').toDate(),
+                };
+            },
+        },
+        {
+            label: 'Prejšnji mesec',
+            type: DateType.LastMonth,
+            callback() {
+                return {
+                    from: moment().add(-1, 'month').startOf('month').toDate(),
+                    to: moment().add(-1, 'month').endOf('month').toDate(),
+                };
+            },
+        },
+        {
+            label: 'Trenutno leto',
+            type: DateType.CurrentYear,
+            callback() {
+                return {
+                    from: moment().startOf('year').toDate(),
+                    to: moment().endOf('year').toDate(),
+                };
+            },
+        },
+        {
+            label: 'Prejšnje leto',
+            type: DateType.LastYear,
+            callback() {
+                return {
+                    from: moment().add(-1, 'year').startOf('year').toDate(),
+                    to: moment().add(-1, 'year').endOf('year').toDate(),
+                };
+            },
+        },
+    ];
+    return (filter && options.filter((x) => filter.some((y) => y == x.type))) || options;
+};
