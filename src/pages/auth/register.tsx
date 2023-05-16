c74276cfccc8ba93339b8eb1cef73d7e6ae475b2
mh-developer <mh7860@student.uni-lj.si>
2023-05-16 21:32:10 +0200

    chore: translate login & register page

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -32,7 +32,7 @@ export default function Register() {
     return (
         <Auth>
             <div className="w-full max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow dark:bg-gray-800">
-                <h2 className="text-2xl font-bold text-gray-900 dark:text-white">Create a Free Account</h2>
+                <h2 className="text-2xl font-bold text-gray-900 dark:text-white">Ustvarite brezplačni račun</h2>
                 <FormProvider {...methods}>
                     <form className="mt-8 space-y-6" action="" onSubmit={handleSubmit(onSubmit)}>
                         <div>
@@ -40,52 +40,52 @@ export default function Register() {
                                 htmlFor="email"
                                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                             >
-                                Your email
+                                Elektronski naslov
                             </label>
                             <input
                                 type="email"
-                                {...register('email', { required: 'Email is required' })}
+                                {...register('email', { required: 'Elektronski naslov je obvezen' })}
                                 name="email"
                                 id="email"
                                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                placeholder="name@mail.com"
+                                placeholder="ime.priimek@mail.com"
                                 required
                             />
-                            {errors.email && <p className="text-red-400">{errors.email.message}</p>}
+                            {errors.email && <p className="mt-2 text-red-400">{errors.email.message}</p>}
                         </div>
                         <div>
                             <label
                                 htmlFor="password"
                                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                             >
-                                Your password
+                                Geslo
                             </label>
                             <input
                                 type="password"
-                                {...register('password', { required: 'Password is required' })}
+                                {...register('password', { required: 'Geslo je obvezno' })}
                                 name="password"
                                 id="password"
                                 placeholder="••••••••"
                                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                 required
                             />
-                            {errors.password && <p className="text-red-400">{errors.password.message}</p>}
+                            {errors.password && <p className="mt-2 text-red-400">{errors.password.message}</p>}
                         </div>
                         <div>
                             <label htmlFor="" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                Confirm password
+                                Potrdi geslo
                             </label>
                             <input
                                 type="password"
                                 {...register('password_confirm', {
-                                    required: 'Verify your password',
+                                    required: 'Potrditev gesla je obvezna',
                                 })}
                                 placeholder="••••••••"
                                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                 required
                             />
                             {errors.password_confirm && (
-                                <p className="text-red-400">{errors.password_confirm.message}</p>
+                                <p className="mt-2 text-red-400">{errors.password_confirm.message}</p>
                             )}
                         </div>
                         <div className="flex items-start">
@@ -101,9 +101,9 @@ export default function Register() {
                             </div>
                             <div className="ml-3 text-sm">
                                 <label htmlFor="remember" className="font-medium text-gray-900 dark:text-white">
-                                    I accept the{' '}
+                                    Sprejmem{' '}
                                     <a href="#" className="text-primary-700 hover:underline dark:text-primary-500">
-                                        Terms and Conditions
+                                        pravila in pogoje uporabe
                                     </a>
                                 </label>
                             </div>
@@ -112,17 +112,17 @@ export default function Register() {
                             type="submit"
                             className="w-full px-5 py-3 text-base font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                         >
-                            Next Step
+                            Nadaljuj
                         </button>
                         <div className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                            Already have an account?{' '}
+                            Že imate račun?{' '}
                             <Link
                                 href={{
                                     pathname: '/auth/login',
                                 }}
                                 className="text-primary-700 hover:underline dark:text-primary-500"
                             >
-                                Login here
+                                Prijavite se tukaj
                             </Link>
                         </div>
                     </form>
