4f2eae87f108cd9b2aae077e3ae52bbfa46fc2db
AmadejKrepek <krepekamadej@gmail.com>
2023-05-17 14:05:43 +0200

    Added some code to develop

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -1,5 +1,6 @@
 import Auth from "@/layouts/Auth";
-import Known from "@/components/Onboarding/Selection/known";
+import Known from "@/components/Onboarding/Selection/Known";
+import Custom from "@/components/Onboarding/Selection/Custom";
 
 export default function Calibration() {
   
@@ -8,10 +9,6 @@ export default function Calibration() {
     <Auth>
       <div className="w-full max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow dark:bg-gray-800">
         <h2 className="text-2xl font-bold text-gray-900 dark:text-white">Dobrodošli! Še zadnji korak...</h2>
-        <div className="text-sm font-medium text-gray-500 dark:text-gray-400">
-          Prosimo, da naslednje podatke izpolnite z največjo možnjo natančnostjo. <br />
-          To nam bo pomagalo, da vam zagotovimo najboljšo izkušnjo.
-        </div>
         <Known />
       </div>
     </Auth>
