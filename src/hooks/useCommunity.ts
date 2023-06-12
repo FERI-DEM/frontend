106af352726eb8e14d27268d338b5bc81326865b
mh-developer <mh7860@student.uni-lj.si>
2023-06-12 21:49:45 +0200

    feat: prepare loading communties and community members with hook

---
+++ src/hooks/useCommunity.ts
@@ -0,0 +1,19 @@
+import CommunityService from '@/api/community.service';
+import { ApiError } from '@/types/common.types';
+import { CommunityRes } from '@/types/community.types';
+import useSWR from 'swr';
+import { CacheKey } from '@/types/caching.types';
+
+const useCommunity = (communityId: string) => {
+    const { data, error, isLoading } = useSWR<CommunityRes, ApiError>(`${CacheKey.COMMUNITY}_${communityId}`, () =>
+        CommunityService.getCommunity(communityId)
+    );
+
+    return {
+        community: data,
+        communityError: error,
+        communityLoading: isLoading,
+    };
+};
+
+export default useCommunity;
