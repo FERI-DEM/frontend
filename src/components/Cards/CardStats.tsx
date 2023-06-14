c3c953f2779211b3703ddc3c68c8d0c2d14aa896
mh-developer <mh7860@student.uni-lj.si>
2023-06-14 20:43:18 +0200

    fix: build error

--- src/components/Cards/CardStats.tsx
+++ src/components/Cards/CardStats.tsx
@@ -1,55 +1,71 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
+interface Props {
+    statSubtitle: string;
+    statTitle: string;
+    statArrow: 'up' | 'down';
+    statPercent: string;
+    // can be any of the text color utilities
+    // from tailwindcss
+    statPercentColor: string;
+    statDescripiron: string;
+    statIconName: string;
+    // can be any of the background color utilities
+    // from tailwindcss
+    statIconColor: string;
+}
 
 export default function CardStats({
-  statSubtitle,
-  statTitle,
-  statArrow,
-  statPercent,
-  statPercentColor,
-  statDescripiron,
-  statIconName,
-  statIconColor,
-}: any) {
-  return (
-    <>
-      <div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg dark:bg-gray-800">
-        <div className="flex-auto p-4">
-          <div className="flex flex-wrap">
-            <div className="relative w-full pr-4 max-w-full flex-grow flex-1">
-              <h5 className="text-slate-400 uppercase font-bold text-xs">{statSubtitle}</h5>
-              <span className="font-semibold text-xl text-slate-700 dark:text-white">{statTitle}</span>
+    statSubtitle,
+    statTitle,
+    statArrow,
+    statPercent,
+    statPercentColor,
+    statDescripiron,
+    statIconName,
+    statIconColor,
+}: Props) {
+    return (
+        <>
+            <div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg dark:bg-gray-800">
+                <div className="flex-auto p-4">
+                    <div className="flex flex-wrap">
+                        <div className="relative w-full pr-4 max-w-full flex-grow flex-1">
+                            <h5 className="text-slate-400 uppercase font-bold text-xs">{statSubtitle}</h5>
+                            <span className="font-semibold text-xl text-slate-700 dark:text-white">{statTitle}</span>
+                        </div>
+                        <div className="relative w-auto pl-4 flex-initial">
+                            <div
+                                className={
+                                    'text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full ' +
+                                    statIconColor
+                                }
+                            >
+                                <span className="material-symbols-rounded">{statIconName}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="text-sm text-slate-400 mt-4">
+                        {statPercent != null ? (
+                            <span className={statPercentColor + ' mr-2'}>
+                                {statArrow === 'up' ? (
+                                    <span className="material-symbols-rounded material-font-size-xs">arrow_upward</span>
+                                ) : statArrow === 'down' ? (
+                                    <span className="material-symbols-rounded material-font-size-xs">
+                                        arrow_downward
+                                    </span>
+                                ) : (
+                                    ''
+                                )}{' '}
+                                {statPercent}
+                            </span>
+                        ) : null}
+                        <span className="whitespace-nowrap text-xs">{statDescripiron}</span>
+                    </p>
+                </div>
             </div>
-            <div className="relative w-auto pl-4 flex-initial">
-              <div
-                className={
-                  'text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full ' +
-                  statIconColor
-                }
-              >
-                <span className="material-symbols-rounded">{statIconName}</span>
-              </div>
-            </div>
-          </div>
-          <p className="text-sm text-slate-400 mt-4">
-            {statPercent != null ? (
-              <span className={statPercentColor + ' mr-2'}>
-                {statArrow === 'up' ? (
-                  <span className="material-symbols-rounded material-font-size-xs">arrow_upward</span>
-                ) : statArrow === 'down' ? (
-                  <span className="material-symbols-rounded material-font-size-xs">arrow_downward</span>
-                ) : (
-                  ''
-                )}{' '}
-                {statPercent}
-              </span>
-            ) : null}
-            <span className="whitespace-nowrap text-xs">{statDescripiron}</span>
-          </p>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
 
 // EXAMPLE OF VALUES
@@ -63,18 +79,3 @@ export default function CardStats({
 //   statIconName: 'bar_chart',
 //   statIconColor: 'bg-green-500',
 // };
-
-CardStats.propTypes = {
-  statSubtitle: PropTypes.string,
-  statTitle: PropTypes.string,
-  statArrow: PropTypes.oneOf(['up', 'down']),
-  statPercent: PropTypes.string,
-  // can be any of the text color utilities
-  // from tailwindcss
-  statPercentColor: PropTypes.string,
-  statDescripiron: PropTypes.string,
-  statIconName: PropTypes.string,
-  // can be any of the background color utilities
-  // from tailwindcss
-  statIconColor: PropTypes.string,
-};
