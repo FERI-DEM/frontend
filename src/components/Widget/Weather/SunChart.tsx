7aa98990501662dfda140a9a3aad2faa95851158
mh-developer <mh7860@student.uni-lj.si>
2023-06-17 00:02:22 +0200

    fix: fix sun chart

--- src/components/Widget/Weather/SunChart.tsx
+++ src/components/Widget/Weather/SunChart.tsx
@@ -3,34 +3,28 @@ import dynamic from 'next/dynamic';
 
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
-export function SunChart({
-    series = [50]
-}: {
-    series: number[];
-}) {
+export function SunChart({ series = [50] }: { series: number[] }) {
     const options = {
         chart: {
             type: 'radialBar' as 'radialBar',
             sparkline: {
-                enabled: true
+                enabled: true,
             },
         },
         colors: ['#E3A008'],
         plotOptions: {
             radialBar: {
-              startAngle: -90,
-              endAngle: 90,
-            }
-          },
-        labels: [''],
-        };
+                startAngle: -90,
+                endAngle: 90,
+                dataLabels: {
+                    show: false,
+                },
+            },
+        },
+    };
     return (
         <>
-            <Chart
-                options={options}
-                series={series}
-                type='radialBar'
-            />
+            <Chart options={options} series={series} type="radialBar" width={'100%'} height={240} />
         </>
     );
 }
