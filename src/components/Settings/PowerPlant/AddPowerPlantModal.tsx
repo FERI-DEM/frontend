d65e4556dabe06834654c42d13cb630b1226f383
AmadejKrepek <krepekamadej@gmail.com>
2023-06-19 12:58:43 +0200

    removeed size

--- src/components/Settings/PowerPlant/AddPowerPlantModal.tsx
+++ src/components/Settings/PowerPlant/AddPowerPlantModal.tsx
@@ -147,24 +147,6 @@ const AddPowerPlantModal = ({ closeModal, updatePowerPlants }: AddPowerPlantModa
                                 {errors.maxPower && <p className="mt-2 text-red-400">{errors.maxPower.message}</p>}
                             </div>
                             <div>
-                                <label
-                                    htmlFor="size"
-                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                >
-                                    Površina (v m<sup>2</sup>)
-                                </label>
-                                <input
-                                    type="text"
-                                    {...register('size', { required: 'Površina elektrarne je obvezno polje' })}
-                                    name="size"
-                                    id="size"
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                    placeholder="Površina"
-                                    required
-                                />
-                                {errors.size && <p className="mt-2 text-red-400">{errors.size.message}</p>}
-                            </div>
-                            <div>
                                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                     Lokacija
                                 </label>
