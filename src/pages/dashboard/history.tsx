71eaab3e0cf3e170e40b46bb235a2ed8bacc92a1
mh-developer <mh7860@student.uni-lj.si>
2023-06-30 00:11:25 +0200

    chore: remove stats from history

--- src/pages/dashboard/history.tsx
+++ src/pages/dashboard/history.tsx
@@ -20,68 +20,6 @@ export default function History() {
             <div className="px-4 pt-6">
                 <ChartHistoryProduction />
             </div>
-            <div className="p-2 flex">
-                <div className="px-2">
-                    <CardStats
-                        statSubtitle="PROIZVODNJA DANES"
-                        statTitle="22 Wh"
-                        statArrow="up"
-                        statPercent="3,48%"
-                        statPercentColor="text-emerald-500"
-                        statDescripiron="Od včeraj"
-                        statIconName="bar_chart"
-                        statIconColor="bg-red-500"
-                    />
-                </div>
-                <div className="px-2">
-                    <CardStats
-                        statSubtitle="PROIZVODNJA DANES"
-                        statTitle="22 Wh"
-                        statArrow="up"
-                        statPercent="3,48%"
-                        statPercentColor="text-emerald-500"
-                        statDescripiron="Od včeraj"
-                        statIconName="bar_chart"
-                        statIconColor="bg-red-500"
-                    />
-                </div>
-                <div className="px-2">
-                    <CardStats
-                        statSubtitle="PROIZVODNJA DANES"
-                        statTitle="22 Wh"
-                        statArrow="up"
-                        statPercent="3,48%"
-                        statPercentColor="text-emerald-500"
-                        statDescripiron="Od včeraj"
-                        statIconName="bar_chart"
-                        statIconColor="bg-red-500"
-                    />
-                </div>
-                <div className="px-2">
-                    <CardStats
-                        statSubtitle="PROIZVODNJA DANES"
-                        statTitle="22 Wh"
-                        statArrow="up"
-                        statPercent="3,48%"
-                        statPercentColor="text-emerald-500"
-                        statDescripiron="Od včeraj"
-                        statIconName="bar_chart"
-                        statIconColor="bg-red-500"
-                    />
-                </div>
-                <div className="px-2">
-                    <CardStats
-                        statSubtitle="PROIZVODNJA DANES"
-                        statTitle="22 Wh"
-                        statArrow="up"
-                        statPercent="3,48%"
-                        statPercentColor="text-emerald-500"
-                        statDescripiron="Od včeraj"
-                        statIconName="bar_chart"
-                        statIconColor="bg-red-500"
-                    />
-                </div>
-            </div>
         </DefaultLayout>
     );
 }
